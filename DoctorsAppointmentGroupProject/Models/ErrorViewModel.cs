1827f3e01593c691304d78b8e18d431a95034095
ShahJahanApurbo <shahapurbo007@gmail.com>
2025-09-17 18:04:20 +0600

    Add project files.

---
+++ DoctorsAppointmentGroupProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DoctorsAppointmentGroupProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
